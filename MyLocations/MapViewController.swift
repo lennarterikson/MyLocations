11809010fc660f328b3c09fe85d9f751ef0b6f91
Lennart Erikson <lennart-sebastian.erikson@commerce-plus.com>
2016-02-02 16:44:03 +0100

    p. 219

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -24,9 +24,12 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func showLocations() {
-        
+        let region = regionForAnnotations(locations)
+        mapView.setRegion(region, animated: true)
     }
     
+    
+    // MARK: - Business logic
     func updateLocations() {
         mapView.removeAnnotations(locations)
         
@@ -39,14 +42,103 @@ class MapViewController: UIViewController {
         mapView.addAnnotations(locations)
     }
     
+    func regionForAnnotations(annotations: [MKAnnotation]) -> MKCoordinateRegion {
+    
+        var region: MKCoordinateRegion
+        
+        switch annotations.count {
+            
+        case 0:
+            region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
+        case 1:
+            let annotation = annotations.last
+            region = MKCoordinateRegionMakeWithDistance(annotation!.coordinate, 1000, 1000)
+        default:
+            var topLeftCoordinate = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRightCoordinate = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            
+            for annotation in annotations {
+                topLeftCoordinate.latitude = max(topLeftCoordinate.latitude, annotation.coordinate.latitude)
+                topLeftCoordinate.longitude = max(topLeftCoordinate.longitude, annotation.coordinate.longitude)
+                
+                bottomRightCoordinate.latitude = max(bottomRightCoordinate.latitude, annotation.coordinate.latitude)
+                bottomRightCoordinate.longitude = max(bottomRightCoordinate.longitude, annotation.coordinate.longitude)
+            }
+            
+            let center = CLLocationCoordinate2D(latitude: topLeftCoordinate.latitude - (topLeftCoordinate.latitude - bottomRightCoordinate.latitude) / 2, longitude: topLeftCoordinate.longitude - (topLeftCoordinate.longitude - bottomRightCoordinate.longitude) / 2)
+            let extraSpace = 1.1
+            
+            let span = MKCoordinateSpan(latitudeDelta: abs(topLeftCoordinate.latitude - bottomRightCoordinate.latitude) * extraSpace, longitudeDelta: abs(topLeftCoordinate.longitude - bottomRightCoordinate.longitude) * extraSpace)
+            
+            region = MKCoordinateRegion(center: center, span: span)
+        }
+        return region
+    }
+    
+    func showLocationDetails(sender: UIButton) {
+        performSegueWithIdentifier("EditLocation", sender: sender)
+    }
+    
+    // MARK: - Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "EditLocation" {
+            
+            let navigationController = segue.destinationViewController as! UINavigationController
+            let controller = navigationController.topViewController as! LocationDetailsViewController
+            
+            let location = locations[(sender as! UIButton).tag]
+            
+            controller.managedObjectContext = managedObjectContext
+            controller.locationToEdit = location
+        }
+    }
+    
+    
+    // MARK: - ViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         updateLocations()
+        
+        if !locations.isEmpty {
+            showLocations()
+        }
     }
     
 }
 
 extension MapViewController: MKMapViewDelegate {
-    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard annotation is Location else {
+            return nil
+        }
+        
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as! MKPinAnnotationView!
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            annotationView.enabled = true
+            annotationView.canShowCallout = true
+            annotationView.animatesDrop = true
+            annotationView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1.0)
+            
+            let rightButton = UIButton(type: .DetailDisclosure)
+            rightButton.addTarget(self, action: Selector("showLocationDetails"), forControlEvents: .TouchUpInside)
+            
+            annotationView.rightCalloutAccessoryView = rightButton
+        } else {
+            annotationView.annotation = annotation
+        }
+        
+        let button = annotationView.rightCalloutAccessoryView as! UIButton
+        if let index = locations.indexOf(annotation as! Location) {
+            button.tag = index
+        }
+        
+        return annotationView
+        
+    }
 }
